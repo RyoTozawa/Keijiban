54349dfe6e34c089b5afbd907f1a39177c5ca79a
RyoTozawa <b1016031@fun.ac.jp>
2018-06-10 14:53:26 +0900

    fix css and php head

--- index.php
+++ index.php
@@ -9,16 +9,17 @@ $count = count($file);
 <link rel="stylesheet/less" type="text/css" href="styles.less">
 <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.0.2/less.min.js" ></script>
 <div class="wrapper">
+    <h1 align="center">掲示板</h1>
+    <!--
     <div class="container">
         <h1>掲示板</h1>
-        <!--
         <form class="form">
             <input type="text" placeholder="Username">
             <input type="password" placeholder="Password">
             <button type="submit" id="login-button">Login</button>
         </form>
-        -->
     </div>
+    -->
     <?php foreach( $file as $row ):?>
     <?php $array = explode('<>', trim($row)); ?>
     <p>
