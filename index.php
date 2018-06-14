9074ede37f96eb0d85e43219f3b09fec0f6aa2fc
RyoTozawa <b1016031@fun.ac.jp>
2018-06-14 20:27:51 +0900

    fix only view

--- index.php
+++ index.php
@@ -1,30 +1,28 @@
 <?php
-include('module.php');
 require_once(dirname(__FILE__).'/module/config.php');
-$target_file = "index.txt";
-//$file = file($target_file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-//$count = count($file);
 ?>
+
 <html>
 <link rel="stylesheet/less" type="text/css" href="styles.less">
 <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.0.2/less.min.js" ></script>
     <div class="wrapper">
-        <h1 align="center">掲示板</h1>
+        <h1 align="center">Forum</h1>
             <form class="form">
-                <button type="button" onclick="location.href='post.php'" style="position: absolute; right: 140px; top: 0px;/">投稿</button>
-                <button type="submit" style="position: absolute; right: 70px; top: 0px;/">消去</button>
-                <button type="button" onclick="location.href='edit.php'" style="position: absolute; right: 0px; top: 0px;/">編集</button>
+                <button type="button" onclick="location.href='index.php'" style="position: absolute; right: 245px; top: 0px;/">Top</button>
+                <button type="button" onclick="location.href='post.php'" style="position: absolute; right: 167px; top: 0px;/">POST</button>
+                <button type="button" onclick="location.href='delete.html'" style="position: absolute; right: 70px; top: 0px;/">DELETE</button>
+                <button type="button" onclick="location.href='edit.html'" style="position: absolute; right: 0px; top: 0px;/">EDIT</button>
             </form>
         <div class="container">
-            <h2 align="center">ログ</h2>
+            <h2 align="center">Log</h2>
                 <?php $record = ORM::for_table('comment')->find_many();?>
                 <?php foreach( $record as $row ):?>
                 <div class="box">
                     <p>
-                        <span><?php echo "投稿番号：".$row->id."</br>" ?></span>
-                        <span><?php echo "投稿者：".$row->user."</br>" ?></span>
-                        <span><?php echo "コメント：".$row->comment."</br>" ?></span>
-                        <span><?php echo "投稿時間：".$row->time."</br>" ?></span>
+                        <span><?php echo "Number：".$row->id."</br>" ?></span>
+                        <span><?php echo "Name：".$row->user."</br>" ?></span>
+                        <span><?php echo "Comment：".$row->comment."</br>" ?></span>
+                        <span><?php echo "Post-Time：".$row->time."</br>" ?></span>
                     </p>
                 </div>
                 <?php endforeach;?>
